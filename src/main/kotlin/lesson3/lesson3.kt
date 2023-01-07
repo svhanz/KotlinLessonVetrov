5fc9544f8eb3f540ec53f0759080e735093a53c2
Sergei Shlegel <serg-69@mail.ru>
2023-01-07 22:39:51 +0300

    KLV-1 variable (added new info) KLV-2 operators (added new info) KVL-3 strings (created package) KVL-4 Boolean operators (created package)

--- src/main/kotlin/lesson3/lesson3.kt
+++ src/main/kotlin/lesson3/lesson3.kt
@@ -1,11 +1,27 @@
 package lesson3
 
 fun main() {
+    // Конкатенация строк
+    val name = "Ivan"
+    val space = ' '
+    println("Привет " + space + name + space + "," + space + "как дела?")
+    // интерполяция строк
+    val lastName = "Ivanov"
+    println("Hi, $name $lastName, you ${40 + 10} years")
     // мультистрока
     val multiString = """ 
-        строка 1
-           строка 2
-         строка 3
-    """.trimIndent() // создает минимальный отступ
+          string_1
+         string_2
+            string_3
+    """.trimIndent() // функция создает отступ равный наименьшему
     println(multiString)
+
+    val multiStringNew = """
+              |string_4
+          |string_5
+            |string_6
+        
+    """.trimMargin() // функция прибивает всё к левому краю
+    println(multiStringNew)
+
 }
