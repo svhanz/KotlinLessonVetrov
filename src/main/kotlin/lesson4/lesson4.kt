895fd8c471393fd68be296cf39e53b365a9f4d0d
Sergei Shlegel <serg-69@mail.ru>
2023-01-09 18:41:45 +0300

    KLV-4 lesson4 added info

--- src/main/kotlin/lesson4/lesson4.kt
+++ src/main/kotlin/lesson4/lesson4.kt
@@ -1,10 +1,15 @@
 package lesson4
 
 fun main() {
-    val ageUser = 42
-    val compareAge = ageUser >= AGE_OF_MAJORITY
-    println(compareAge)
+    val ageUser = 66
+    //val compareAge = (ageUser >= AGE_OF_MAJORITY) and (ageUser <= RETIREMENT_AGE)
+    // сокращенная запись этого же выражения
+    val compareAge = ageUser in AGE_OF_MAJORITY.. RETIREMENT_AGE
+
+    println("Проверка рабочего возраста: $compareAge")
+
 }
 
 const val AGE_OF_MAJORITY = 18
+const val RETIREMENT_AGE = 65
 
