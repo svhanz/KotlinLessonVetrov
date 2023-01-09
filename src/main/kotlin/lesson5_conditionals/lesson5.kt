62ae23b748047e524238af87cb746454464f8b88
Sergei Shlegel <serg-69@mail.ru>
2023-01-09 23:19:10 +0300

    KLV-5 lesson5 added info

--- src/main/kotlin/lesson5_conditionals/lesson5.kt
+++ src/main/kotlin/lesson5_conditionals/lesson5.kt
@@ -1,5 +1,20 @@
+@file:Suppress("DEPRECATED_IDENTITY_EQUALS")
+
 package lesson5_conditionals
 
 fun main() {
-
+    conditionalIf()
 }
+fun conditionalIf() {
+    val ageUser = readln().toInt()
+    if (ageUser == AGE_OF_MAJORITY) {
+        println("Вход разрешен")
+    } else if (ageUser in 20 .. 40){
+        println("Вполне себе взрослый чувак")
+    }
+    else {
+        println("Вход запрещен")
+    }
+}
+
+const val AGE_OF_MAJORITY = 18
